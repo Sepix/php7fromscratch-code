813333ba56bc0e49933000b114e082c77196819c
Thomas Pichler <thomas@thomaspichler.me>
2018-08-22 18:57:07 -0700

    Re-Commit Code

---
+++ chapter-5/variable_string.php
@@ -0,0 +1,14 @@
+<?php
+
+// Outputs My name is Thomas
+
+$name = ‘Thomas’;
+
+echo 'My Name is ' . $name;
+
+
+// Using the Variable inside a String instead
+
+$name = 'Thomas';
+
+echo “My Name is {$name}”;
---
+++ chapter-5/variable_string2.php
@@ -0,0 +1,11 @@
+<?php
+
+// Outputs She is beautiful
+
+$adjective = ‘beautiful’;
+
+echo “She is {$adjective}”
+
+// But what about if she sings beautifully?
+
+echo “She sings {$adjective}ly”;
